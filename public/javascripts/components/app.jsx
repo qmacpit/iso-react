9eb653fcd2327b31cf925f04860f92e637a8965c
qmacpit <macpit1@yahoo.com>
2015-08-25 20:48:54 +0200

    cleanup

--- public/javascripts/components/app.jsx
+++ public/javascripts/components/app.jsx
@@ -2,31 +2,19 @@ var React = require("react"),
     Search = require("./search"),
     Feeders = require("./feeders");
 
-// var feeders = [
-//     {
-//         id: "F111"
-//     },
-//     {
-//         id: "F222"
-//     }
-// ]
-
 var App = React.createClass({
   getInitialState() {
-    console.log("1");
-    console.log(this.props);
     
     if (typeof window !== "undefined") {      
       if (typeof dataCache !== undefined)
        this.props.feeders = dataCache.feeders;
     }
 
-    return {feeders: this.props.feeders};      
+    return {
+      feeders: this.props.feeders
+    };
   },
   render() {
-     console.log("2");
-    console.log(this.props);
-
     return (
         <div>
           <Search />
--- public/javascripts/components/feeders.jsx
+++ public/javascripts/components/feeders.jsx
@@ -3,14 +3,14 @@ var React = require("react");
 var Feeders = React.createClass({
    render() {    
     return (
-        <ul >
-            {
-                    this.props.data 
-                    ? this.props.data.map((function(feeder) {
-                        return <li key={feeder.id} onClick={this.feederSelected}>{feeder.id}</li>
-                    }).bind(this))
-                    : ""
-            }
+        <ul>
+          {
+            this.props.data 
+            ? this.props.data.map((function(feeder) {
+                return <li key={feeder.id} onClick={this.feederSelected}>{feeder.id}</li>
+            }).bind(this))
+            : ""
+          }
         </ul>
     );
   },
